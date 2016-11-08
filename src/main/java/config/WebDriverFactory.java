919c65ea134b876b14176a5ae3b517dd477aac94
SanchelliosProg <vasyania2009@gmail.com>
2016-11-09 01:26:43 +0300

    - added windows 64 drivers

--- src/main/java/config/WebDriverFactory.java
+++ src/main/java/config/WebDriverFactory.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Created by alex on 22.10.16.
  */
 public class WebDriverFactory {
+    private final static int LINUX = 0;
+    private final static int WINDOWS_10 = 1;
+    private final static String DIR = "./resources/drivers/";
 
     public static WebDriver getDriver(int id){
         switch (id){
@@ -21,12 +24,44 @@ public class WebDriverFactory {
     }
 
     private static WebDriver getChromeDriver(){
+        String driverName = "webdriver.chrome.driver";
+
+        switch (getSystemName()){
+            case WINDOWS_10:
+                System.setProperty(driverName, DIR + "windows_10/chromedriver.exe");
+                break;
+            case LINUX:
+                System.setProperty(driverName, DIR + "linux/chromedriver.exe");
+                break;
+            default:
+                break;
+        }
         return new ChromeDriver();
     }
 
     private static WebDriver getFirefoxDriver(){
-        System.setProperty("webdriver.gecko.driver",
-                "./resources/drivers/geckodriver");
+        String driverName = "webdriver.gecko.driver";
+        switch (getSystemName()){
+            case WINDOWS_10:
+                System.setProperty(driverName,
+                        DIR + "windows_10/geckodriver.exe");
+                break;
+            case LINUX:
+                System.setProperty(driverName,
+                        DIR + "linux/geckodriver");
+                break;
+            default:
+                break;
+        }
+
         return new FirefoxDriver();
+    }
+
+    private static int getSystemName() {
+        if(System.getProperty("os.name").equals("Windows 10")){
+            return WINDOWS_10;
+        }else {
+            return LINUX;
+        }
     }
 }
